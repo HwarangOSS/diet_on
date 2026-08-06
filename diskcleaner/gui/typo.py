7fddd7c2be7d2db428a581b648121ca18ed6f0b3
sumin <sumin50240585@gmail.com>
2026-08-06 09:04:56 +0900

    ♻️refact : 타이포 토큰 정리

--- diskcleaner/gui/typo.py
+++ diskcleaner/gui/typo.py
@@ -1,19 +1,16 @@
 # 디자인 토큰
 from PySide6.QtGui import QFont
-
 from .fonts import load_app_fonts
 
 # 전역 반응형
 _current_scale = 1.0
 BASE_WINDOW_WIDTH = 500
 
-
 # 폰트 비율
 def set_global_scale(window_width: int):
     global _current_scale
     scale = window_width / BASE_WINDOW_WIDTH
     _current_scale = max(0.6, min(scale, 1.5))
-
 
 class FontFamily:
     PLAY_REGULAR = "Play-Regular"
@@ -28,7 +25,6 @@ class FontFamily:
     PRETENDARD_EXTRABOLD = "Pretendard-ExtraBold"
     PRETENDARD_BLACK = "Pretendard-Black"
 
-
 def get_font(family_key: str, size: int, bold: bool = False) -> QFont:
     families = load_app_fonts()
     family_name = families.get(family_key, "Arial")
@@ -37,46 +33,41 @@ def get_font(family_key: str, size: int, bold: bool = False) -> QFont:
     font.setBold(bold)
     return font
 
-
-# 메인 타이틀 / 로딩 / 클릭 / 남은 GB
-def play_large() -> QFont:
+# Play ----------------------------------
+# 메인 타이틀 / 로딩 / 클릭
+def Headline() -> QFont:
     return get_font(FontFamily.PLAY_REGULAR, 20)
 
-
-def titlebar_title() -> QFont:
-    return get_font(FontFamily.PLAY_BOLD, 12)
-
-
-# Delete & Duplicate 소제목
-def play_medium() -> QFont:
-    return get_font(FontFamily.PLAY_REGULAR, 12)
-
-
-# 각 리스트 파일명
-def body_mini_title() -> QFont:
-    return get_font(FontFamily.PRETENDARD_REGULAR, 12)
-
-
-# 기본 폰트(미분류 된거)
-def body() -> QFont:
-    return get_font(FontFamily.PRETENDARD_REGULAR, 8)
-
-
-# 항목별 용량
-def body_medium() -> QFont:
-    return get_font(FontFamily.PRETENDARD_REGULAR, 8)
-
+# 삭제 & 중복 소제목 
+def headline_small() -> QFont:
+    return get_font(FontFamily.PLAY_REGULAR, 16)
 
 # 해시태그
 def hashtag() -> QFont:
-    return get_font(FontFamily.PRETENDARD_REGULAR, 4)
+    return get_font(FontFamily.PLAY_REGULAR, 6)
 
+# 신뢰도
+def trust() -> QFont:
+    return get_font(FontFamily.PLAY_REGULAR, 4)
 
-# 중복항목 묶기 타이틀
-def duplicate() -> QFont:
-    return get_font(FontFamily.PRETENDARD_BOLD, 12)
+# Pretendard ----------------------------------
 
+# 파일명
+def naming() -> QFont:
+    return get_font(FontFamily.PRETENDARD_REGULAR, 20)
 
-# 중복항목 묶기 타이틀
-def caption() -> QFont:
+# 그룹화
+def group() -> QFont:
+    return get_font(FontFamily.PRETENDARD_BOLD, 8)
+
+# 기본 텍스트 & 파일 경로
+def path() -> QFont:
+    return get_font(FontFamily.PRETENDARD_REGULAR, 8)
+
+# 용량 표시 & 더보기
+def mini() -> QFont:
+    return get_font(FontFamily.PRETENDARD_REGULAR, 6)
+
+# 버전
+def ver() -> QFont:
     return get_font(FontFamily.PRETENDARD_REGULAR, 4)
